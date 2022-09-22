1bc63f9fe6283b2f4caffda71b118ebe2597a554
bradly0cjw <bradly093@gmail.com>
2022-09-22 19:45:01 +0800

    Week 2

---
+++ w2 crazy.py
@@ -0,0 +1,24 @@
+def crazy():
+    birth=int(input())
+    var1=birth%10000
+    var2=var1//100
+    var3=var1%100
+    var4=int(0)
+    opt=str("")
+    if (var2 > 9):
+        var4=var4+1 
+    if ((var3%2) == 1):
+        var4=var4+2 
+    if(var4 == 1):
+        opt=("科學麵")
+    elif(var4 == 2):
+        opt=("飲料")
+    elif(var4 == 3):
+        opt=("科學麵+飲料")
+    else:
+        opt=("0")
+    
+    print(opt)
+
+while True:
+    crazy()
