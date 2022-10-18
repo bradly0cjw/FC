073615c62bf1317bcfa4bce7f46e1308c7260c65
bradly0cjw <bradly093@gmail.com>
2022-10-18 14:30:06 +0800

    python test

--- w4-2.py
+++ w4-2.py
@@ -9,9 +9,10 @@ while True:
         denormalize=exponent
         exponent=0
     num2=(((abs(eval)/(2**(num-denormalize))))*(2**denormalize))%1
-    for i in range(0,23):
-        num3=(num2*2//1,num2*2%1)
-        num2=num3[1]
-        binary.append(int(num3[0]))
-    x=''.join(str(x) for x in binary)
-    print(x)
+
+    # for i in range(0,23):
+    #     num3=(num2*2//1,num2*2%1)
+    #     num2=num3[1]
+    #     binary.append(int(num3[0]))
+    # x=''.join(str(x) for x in binary)
+    # print(x)
---
+++ w5.py
@@ -0,0 +1,4 @@
+regiester=[0 for i in range(16)]
+memory=[0 for i in range(256)]
+ir=32
+print(f'{ir:032b}')
