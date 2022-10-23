5892b3e595ce01fe1831335574b5f30ff63a36fa
bradly0cjw <bradly093@gmail.com>
2022-10-23 18:10:40 +0800

    test

--- w4-2.py
+++ w4-2.py
@@ -1,14 +1,18 @@
 import math
 while True:
-    denormalize=0
     eval=float(input())
     num=int((math.log(abs(float(eval)),2))//1)
-    exponent=127+num
-    if num<-127:
-        denormalize=exponent
-        exponent=0
-    num2=(((abs(eval)/(2**(num-denormalize))))*(2**denormalize))%1
-
+    # no need in this section 
+    # denormalize=0
+    # exponent=127+num
+    # if num<-127:
+    #     denormalize=exponent
+    #     exponent=0
+    # num2=int(((((abs(eval)/(2**(num-denormalize))))*(2**denormalize))%1)*(2**23))
+    num2=int((((abs(eval)/(2**num)))%1)*(2**23)) 
+    print(f'{num2:023b}')
+    # num3=int((abs(eval))%1*(2**(23-num+denormalize)))
+    # num2=(((abs(eval)/(2**(num-denormalize))))*(2**denormalize))%1
     # binary=[]
     # for i in range(0,23):
     #     num3=(num2*2//1,num2*2%1)
--- w4-3.py
+++ w4-3.py
@@ -1,24 +1,27 @@
 import math
 while True:
-    denormalize=0
     eval=float(input())
     num=int((math.log(abs(float(eval)),2))//1)
     exponent=127+num
     if num<-127:
         denormalize=exponent
         exponent=0
-    num2=int(((((abs(eval)/(2**(num-denormalize))))*(2**denormalize))%1)*(2**23))
-    x=(f'{num2:023b}') 
-    y=(f'{exponent:08b}')
+    else:
+        denormalize=0
+    num2=int(((abs(eval)/(2**(num-(2*denormalize))))%1)*(2**23))
+    # num4=int(((abs(eval))*(2**(23))))
+    # x=(f'{num2:023b}') 
+    # z=(f'{num3:023b}')
+    # y=(f'{exponent:08b}')
     if (eval<0):
         sign='1'
     else:
         sign='0'
-    print(sign+y+x)
+    print(sign+f'{exponent:08b}'+f'{num2:023b}')
     # binary=[]
     # expon=[]
     # for i in range(0,8):
-    #     calc=(exponent//(2**(7-i)),exponent%(2**(7-i)))
+    #     calc=(exponent//(2**(7-i)),expon-ent%(2**(7-i)))
     #     expon.append(int(calc[0]))
     #     exponent=calc[1]
     # y=''.join(str(y) for y in expon)
