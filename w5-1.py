9477836892f5755346ef2cacba1aad12c1f07716
bradly0cjw <bradly093@gmail.com>
2022-10-25 00:12:42 +0800

    optimize

--- w5-1.py
+++ w5-1.py
@@ -1,21 +1,25 @@
-maxp=1000000
-li=[True]*maxp
-for i in range(2,int((maxp**0.5)+1)):
-    if li[i]==True:
-        li[i]=i
-        for j in range(i*i,(maxp),i):
-            if (li[j]==True):
-                li[j]=i
-for i in range(0,maxp):
-    if li[i]==True:
-        li[i]=i
-while True:
-    k=input().split()
-    o=[]
-    for l in k:
-        o.append(str(li[int(l)]))
-    s=" ".join(o)  
-    print(s)
+def main():
+    maxp=1000000
+    li=[True]*maxp
+    for i in range(2,int((maxp**0.5)+1)):
+        if li[i]==True:
+            li[i]=i
+            for j in range(i*i,(maxp),i):
+                if (li[j]==True):
+                    li[j]=i
+    # for i in range(0,maxp):
+    #     if li[i]==True:
+    #         li[i]=i
+    while True:
+        k=input().split()
+        o=[]
+        for l in k:
+            if li[int(l)]==True:
+                li[int(l)]=l
+            o.append(str(li[int(l)]))
+        s=" ".join(o)  
+        print(s)
+main()
 # print(li[int(k)],k)
 # for i in range(2,maxp):
 #     if li[i]:
--- w5-3.py
+++ w5-3.py
@@ -1,27 +1,29 @@
 import collections
-maxp=1000000
-li=[True]*maxp
-for i in range(2,int((maxp**0.5)+1)):
-    if li[i]==True:
-        li[i]=i
-        for j in range(i*i,(maxp),i):
-            if (li[j]==True):
-                li[j]=i
-for i in range(0,maxp):
-    if li[i]==True:
-        li[i]=i
-while True:
-    k=j=input()
-    out=[]
-    v=1
-    li2=li[int(k)]
-    while li2!=1:
-        out.append(str(li2))
-        k=int(k)/li2
-        li2=li[int(k)] 
-    # if len(out)<1:
-    #     out.append(str(1))
-    n=list(set(out))
-    for m in n:
-        v=v*(collections.Counter(out)[m]+1)  
-    print(v) 
+def main():
+    maxp=1000000
+    li=[True]*maxp
+    for i in range(2,int((maxp**0.5)+1)):
+        if li[i]==True:
+            li[i]=i
+            for j in range(i*i,(maxp),i):
+                if (li[j]==True):
+                    li[j]=i
+    for i in range(0,maxp):
+        if li[i]==True:
+            li[i]=i
+    while True:
+        k=j=input()
+        out=[]
+        v=1
+        li2=li[int(k)]
+        while li2!=1:
+            out.append(str(li2))
+            k=int(k)/li2
+            li2=li[int(k)] 
+        # if len(out)<1:
+        #     out.append(str(1))
+        n=list(set(out))
+        for m in n:
+            v=v*(collections.Counter(out)[m]+1)  
+        print(v) 
+main()
