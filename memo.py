4ab7a34867087ab374c33470b12ebbe1b1bdf1f1
bradly0cjw <bradly093@gmail.com>
2022-11-07 17:16:49 +0800

    memo

--- memo.py
+++ memo.py
@@ -1,4 +1,7 @@
-regiester=[0 for i in range(16)]
-memory=[0 for i in range(256)]
-ir=32
-print(f'{ir:032b}')
+# regiester=[0 for i in range(16)]
+# memory=[0 for i in range(256)]
+# ir=32
+# print(f'{ir:032b}')
+a=int(11)
+c=a<<1
+print(bin(c))
