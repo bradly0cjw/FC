4baa383a51506129d3ed25f742ec422b98ea00b1
bradly0cjw <bradly093@gmail.com>
2022-11-14 16:20:07 +0800

    stuff

--- Week8/course.py
+++ Week8/course.py
@@ -1,6 +1,12 @@
-def selectionsort(data,wall):
-    if wall==len(data)-2:
-        return
-    else:
-        m=min(data[wall:])
-        idx=data.indecx(m,wall)
+def selectionsort(data):
+    for wall in range(len(data)-1):
+        m=10000000000
+        for i in range(wall,len(data)):
+            if data[i]<m:
+                m=data[i];
+                idx=i;
+            data[wall],data[idx]=data[idx],idx[wall]
+        return data
+unsorted=[2,9,4,8,5,1,7,3,6];
+sort=selectionsort(unsorted);
+print(sort) 
