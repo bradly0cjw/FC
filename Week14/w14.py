80d54192d5e20efd5dadc6c04b0c7800ed27d985
bradly0cjw <bradly093@gmail.com>
2022-12-27 17:41:32 +0800

    final update

--- Week14/w14.py
+++ Week14/w14.py
@@ -1,5 +1,5 @@
-# a=input()
-# a=list(map(int,a.split(" ")))
+a=input()
+a=list(map(int,a.split(" ")))
 global bstarray
 bstarray=[]
 class Node:
@@ -39,23 +39,39 @@ def bsttoarray(tree,num):
         return None
     if tree.key==None:
         return None
-    bstarray[num-1]=(tree.key)
+    bstarray[num]=(tree.key)
     bsttoarray(tree.lptr,2*(num))
     bsttoarray(tree.rptr,2*(num)+1)
     return bstarray
     pass
 bst=None
 # a=[8,3,1,6,4,7,10,14,13]
-a=[9,5,10,3,6,14]
+# a=[9,5,10,3,6,14]
 arraylength=0
 for i in a:
     bst=insertBST(bst,i)
 length=maxbst(bst)
 for k in range(0,length):    
     arraylength=arraylength+2**k
-bstarray=[None]*(arraylength)
-print(bsttoarray(bst,1))
-
+bstarray=[None]*(arraylength+1)
+bsttoarray(bst,1)
+base=arraylength-2**(length-1)
+for j in range(1,2**(length-1)+1):
+    calc=base+j
+    temparray=[]
+    if bstarray[calc]!=None:
+        temparray.append(bstarray[calc])
+    while calc!=1:
+        if (calc%2)!=0:
+            calc=calc-1
+        calc=int(calc/2)
+        if bstarray[calc]==None:
+            break
+        temparray.append(bstarray[calc])
+    if len(temparray)==length:
+        for i in range(len(temparray)-1,0,-1):
+            print(temparray[i],end=" ")
+        print(temparray[0])
 # def heightBST(tree):
 #     if tree==None:
 #         return 0
