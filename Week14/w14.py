821ad32f40cdb70412d80d17a10b226d0d756153
bradly0cjw <bradly093@gmail.com>
2022-12-27 17:15:25 +0800

    bst array

--- Week14/w14.py
+++ Week14/w14.py
@@ -1,5 +1,7 @@
 # a=input()
 # a=list(map(int,a.split(" ")))
+global bstarray
+bstarray=[]
 class Node:
     def __init__(self,key):
         self.key=key
@@ -31,13 +33,29 @@ def insertBST(tree,target):
 def maxbst(tree):
     if tree==None:
         return 0
-    print("tree",tree.key)
     return(max(1+maxbst(tree.rptr),1+maxbst(tree.lptr)))
+def bsttoarray(tree,num):
+    if tree==None:
+        return None
+    if tree.key==None:
+        return None
+    bstarray[num-1]=(tree.key)
+    bsttoarray(tree.lptr,2*(num))
+    bsttoarray(tree.rptr,2*(num)+1)
+    return bstarray
+    pass
 bst=None
-a=[8,3,1,6,4,7,10,14,13]
+# a=[8,3,1,6,4,7,10,14,13]
+a=[9,5,10,3,6,14]
+arraylength=0
 for i in a:
     bst=insertBST(bst,i)
-print(searchBST(bst,4))
+length=maxbst(bst)
+for k in range(0,length):    
+    arraylength=arraylength+2**k
+bstarray=[None]*(arraylength)
+print(bsttoarray(bst,1))
+
 # def heightBST(tree):
 #     if tree==None:
 #         return 0
